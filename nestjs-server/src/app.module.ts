ac50c7d5953268605115da1518d52e915b28de80
thachdinh110902@gmail.com <thachdinh@ThachNhe>
2024-08-12 15:11:44 +0700

    add logging https request

--- nestjs-server/src/app.module.ts
+++ nestjs-server/src/app.module.ts
@@ -1,14 +1,16 @@
-import { Module } from '@nestjs/common';
+import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { ConfigModule } from '@nestjs/config';
 import { MinioClientModule } from './minio-client/minio-client.module';
 import { FileUploadModule } from './file-upload/file-upload.module';
-
+import { LoggerMiddleware } from './utils/logger.middleware';
 @Module({
   imports: [AuthModule, UserModule, PrismaModule, ConfigModule.forRoot({ isGlobal: true }), MinioClientModule, FileUploadModule],
 })
-export class AppModule {
-
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
 }
--- nestjs-server/src/main.ts
+++ nestjs-server/src/main.ts
@@ -1,10 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-
-
 declare const module: any;
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
