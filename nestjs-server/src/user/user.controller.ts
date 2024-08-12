802986e23580d9aa3d57bcf61c59ffe0926ece9d
thachdinh110902@gmail.com <thachdinh@ThachNhe>
2024-08-12 09:46:30 +0700

    add env for docker compose

--- nestjs-server/src/user/user.controller.ts
+++ nestjs-server/src/user/user.controller.ts
@@ -1,13 +1,18 @@
+import { UserService } from './user.service';
 import { Controller, Get, UseGuards, } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
-@Controller('user')
+@Controller()
 export class UserController {
 
-   @UseGuards(AuthGuard('jwt'))
-   @Get("details")
-   get() {
-      return "user detail";
+   // @UseGuards(AuthGuard('jwt'))
+   // @Get("details")
+   // get() {
+   //    return "user detail";
+   // }
+   constructor(private userService: UserService) { }
+   @Get('users')
+   async getUsers() {
+      return this.userService.getUsersService()
    }
-
 }
 
--- nestjs-server/src/user/user.service.ts
+++ nestjs-server/src/user/user.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
-
+import { PrismaService } from '../prisma/prisma.service';
 @Injectable()
-export class UserService {}
+
+export class UserService {
+   constructor(private prismaService: PrismaService) { }
+   getUsersService() {
+      const users = this.prismaService.user.findMany();
+      console.log("check users : ", users);
+   }
+}
