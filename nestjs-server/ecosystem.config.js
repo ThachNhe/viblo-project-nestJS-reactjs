c0766e4833dbfcc6ff336244d673e048ad0660e7
thachdinh110902@gmail.com <thachdinh@ThachNhe>
2024-10-07 13:52:28 +0700

    load test with pm2+docker

--- nestjs-server/ecosystem.config.js
+++ nestjs-server/ecosystem.config.js
@@ -2,14 +2,10 @@ module.exports = {
   apps: [
     {
       name: 'nestjs-app',
-      script: 'node_modules/.bin/ts-node',
-      args: '-r tsconfig-paths/register src/main.ts',
-      watch: ['src'], // Theo dõi các thay đổi trong thư mục src
-      ignore_watch: ['node_modules', 'logs'], // Bỏ qua node_modules và logs
+      script: 'dist/main.js',
+      instances: 2,
+      exec_mode: 'cluster',
       env: {
-        NODE_ENV: 'development',
-      },
-      env_production: {
         NODE_ENV: 'production',
       },
     },
